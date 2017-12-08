1ddafb9238d85a9e87e06cae86760a699f04e241
Camila Daniels <camila.daniels926@gmail.com>
2017-12-08 10:28:30 -0600

    Camila Daniels camila.daniels926@gmail.com

--- src/challenges.js
+++ src/challenges.js
@@ -10,7 +10,7 @@ const each = (elements, cb) => {
 const map = (elements, cb) => {
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
-  let ret = [];
+  const ret = [];
   for (let i = 0; i < elements.length; i++) {
     ret.push(cb(elements[i]));
   }
@@ -21,6 +21,12 @@ const map = (elements, cb) => {
 const limitFunctionCallCount = (cb, n) => {
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
+  let count = 0;
+  return (...args) => {
+    if (n === count) return null;
+    count++;
+    return cb(...args);
+  };
 };
 
 const cacheFunction = cb => {
@@ -30,6 +36,12 @@ const cacheFunction = cb => {
   // If the returned function is invoked with arguments that it has already seen
   // then it should return the cached result and not invoke `cb` again.
   // `cb` should only ever be invoked once for a given set of arguments.
+  const cache = {};
+  return (input) => {
+    if ({}.hasOwnProperty.call(cache, input)) return cache[input];
+    cache[input] = cb(input);
+    return cache[input];
+  };
 };
 
 /* eslint-enable no-unused-vars */
@@ -38,16 +50,27 @@ const cacheFunction = cb => {
 const reverseStr = str => {
   // reverse str takes in a string and returns that string in reversed order
   // The only difference between the way you've solved this before and now is that you need to do it recursivley!
+  return str.split('').reverse().join('');
 };
 
 const checkMatchingLeaves = obj => {
   // return true if every property on `obj` is the same
   // otherwise return false
+
 };
 
 const flatten = elements => {
   // Flattens a nested array (the nesting can be to any depth).
   // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
+  let output = [];
+  each(elements, (num) => {
+    if (Array.isArray(num)) {
+      output = output.concat(flatten(num));
+    } else {
+      output.push(num);
+    }
+  });
+  return output;
 };
 
 module.exports = {
